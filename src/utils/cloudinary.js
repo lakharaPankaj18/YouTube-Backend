c62628f378af35edc6638172d59cb9281a620565
pankaj <ipankajlakhara49@gmail.com>
2025-08-30 17:12:20 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -24,14 +24,14 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-cloudinary.v2.uploader.upload(
-  "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-  {
-    public_id: "shoes",
-  },
-  function (error, result) {
-    console.log(result);
-  }
-);
+// cloudinary.v2.uploader.upload(
+//   "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//   {
+//     public_id: "shoes",
+//   },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
 
 export { uploadOnCloudinary };
